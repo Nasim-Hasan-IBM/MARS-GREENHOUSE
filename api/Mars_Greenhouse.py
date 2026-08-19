f4d94b744d6ce8e72e234c2369a08896799678ec
Nasim-Hasan <nasimcse2k5@gmail.com>
2026-08-19 15:14:42 +0600

    Modified Mars_Greenhouse front-end (HTML) and back-end (Python) Scripts

--- api/Mars_Greenhouse.py
+++ api/Mars_Greenhouse.py
@@ -19,7 +19,7 @@ from ibm_watsonx_ai.metanames import GenTextParamsMetaNames as GenParams
 app = Flask(__name__)
 CORS(app)  # remove or restrict in production
 
-# 1. Load Credentials 
+# 1. Load Credentials & Global Variables
 load_dotenv("Secrets.env")
 NASA_API_KEY = os.environ["NASA_API_KEY"]
 CLOUDANT_API_KEY = os.environ["CLOUDANT_API_KEY"]
@@ -28,44 +28,42 @@ CLOUDANT_DB = os.environ["CLOUDANT_DB"]
 WATSONX_API_KEY = os.environ["WATSONX_API_KEY"]
 WATSONX_URL = os.environ["WATSONX_URL"]
 WATSONX_PROJECT_ID = os.environ["WATSONX_PROJECT_ID"]
+creds = Credentials(
+api_key=os.environ["WATSONX_API_KEY"],
+url=os.environ["WATSONX_URL"],
+)
+project_id = os.environ["WATSONX_PROJECT_ID"]
 
-#2. Fetching Information from NASA's InSight MARS API
-resp = requests.get("https://api.nasa.gov/insight_weather/?api_key="+NASA_API_KEY+"&feedtype=json&ver=1.0")
-resp.raise_for_status() 
-
-#3. Storing those Information into the IBM Cloudant
-# Setting Up Authenticator with the IBm Cloud API Key
-authenticator = IAMAuthenticator(CLOUDANT_API_KEY)
-# Initializing the Cloudant Client with the Service URL 
-client = CloudantV1(authenticator=authenticator)
-client.set_service_url(CLOUDANT_SERVICE_URL)
-
-#4. Inserting Documents in the Cloudant
-try:
-    # This Generates an Unique Server-Side Document ID Automatically
-    response = client.post_document(
-            db=CLOUDANT_DB,
-            document=resp.json()
-        ).get_result()
-    print("Success! Document inserted.")
-
-#5. Capturing the Exceptions
-except ApiException as ae:
-    print(f"IBM Cloudant API exception occurred: {ae.code} - {ae.message}")
-except Exception as e:
-    print(f"An unexpected error occurred: {e}")
-
-#6. Predict Irrigation in MARS
+#2-a. Predict Irrigation in MARS
 @app.get("/api/irrigation")
 def predict_irrigation():
-    # Setting Up the Required Parameters : WATSONX API KEY, WATSONX URL, WATSONX PROJECT ID
-    creds = Credentials(
-    api_key=os.environ["WATSONX_API_KEY"],
-    url=os.environ["WATSONX_URL"],
-    )
-    project_id = os.environ["WATSONX_PROJECT_ID"]
+    #3. Fetching Information from NASA's InSight MARS API
+    resp = requests.get("https://api.nasa.gov/insight_weather/?api_key="+NASA_API_KEY+"&feedtype=json&ver=1.0")
+    resp.raise_for_status() 
 
-    # Init Client + Picking Up a Granite Model for the account
+    #4. Storing those Information into the IBM Cloudant
+    # Setting Up Authenticator with the IBm Cloud API Key
+    authenticator = IAMAuthenticator(CLOUDANT_API_KEY)
+    # Initializing the Cloudant Client with the Service URL 
+    client = CloudantV1(authenticator=authenticator)
+    client.set_service_url(CLOUDANT_SERVICE_URL)
+
+    #5. Inserting Documents in the Cloudant
+    try:
+        # This Generates an Unique Server-Side Document ID Automatically
+        response = client.post_document(
+                db=CLOUDANT_DB,
+                document=resp.json()
+            ).get_result()
+        print("Success! Document inserted.")
+    
+    #6. Capturing the Exceptions
+    except ApiException as ae:
+        print(f"IBM Cloudant API exception occurred: {ae.code} - {ae.message}")
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
+    
+    #7. Init Client + Picking Up a Granite Model for the account
     client = APIClient(credentials=creds, project_id=project_id)
     params = {
     GenParams.MAX_NEW_TOKENS: 300,
@@ -78,17 +76,116 @@ def predict_irrigation():
     credentials=creds,
     project_id=project_id,
     )
-    
-    # Prompt: using a JSON 
+   
+    #8. Prompt: using a JSON and returning
     prompt=f"""
-    You are a space scientist. So, could you please predict the irrigation of Mars with the JSON data: {resp.json()} 
+    You are a space scientist. So, could you please analyze the data {resp.json()} and predict the irrigation in Mars?
     """
     res = model.generate(prompt=prompt)
-    #print(res)
-    text = res["results"][0]["generated_text"].strip()
-    print (text)
-    return text
-    
+    print(res["results"][0]["generated_text"])
+    return res
 
+#2-b. Predict Diseases in MARS
+@app.get("/api/disease")
+def predict_disease():
+    #3. Fetching Information from NASA's InSight MARS API
+    resp = requests.get("https://api.nasa.gov/insight_weather/?api_key="+NASA_API_KEY+"&feedtype=json&ver=1.0")
+    resp.raise_for_status() 
+
+    #4. Storing those Information into the IBM Cloudant
+    # Setting Up Authenticator with the IBm Cloud API Key
+    authenticator = IAMAuthenticator(CLOUDANT_API_KEY)
+    # Initializing the Cloudant Client with the Service URL 
+    client = CloudantV1(authenticator=authenticator)
+    client.set_service_url(CLOUDANT_SERVICE_URL)
+
+    #5. Inserting Documents in the Cloudant
+    try:
+        # This Generates an Unique Server-Side Document ID Automatically
+        response = client.post_document(
+                db=CLOUDANT_DB,
+                document=resp.json()
+            ).get_result()
+        print("Success! Document inserted.")
+    
+    #6. Capturing the Exceptions
+    except ApiException as ae:
+        print(f"IBM Cloudant API exception occurred: {ae.code} - {ae.message}")
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
+    
+    #7. Init Client + Picking Up a Granite Model for the account
+    client = APIClient(credentials=creds, project_id=project_id)
+    params = {
+    GenParams.MAX_NEW_TOKENS: 300,
+    GenParams.TEMPERATURE: 0.2,     # low temp for more factual outputs
+    GenParams.TOP_P: 0.9,
+    }
+    model = ModelInference(
+    model_id="ibm/granite-4-h-small",  # or another Granite instruct model you have access to
+    params=params,
+    credentials=creds,
+    project_id=project_id,
+    )
+   
+    #8. Prompt: using a JSON and returning
+    prompt=f"""
+    You are a space scientist. So, could you please analyze the data {resp.json()} and predict the diseases in Mars?
+    """
+    res = model.generate(prompt=prompt)
+    print(res["results"][0]["generated_text"])
+    return res
+
+#2-c. Predict Energy Optimization in MARS
+@app.get("/api/energy")
+def predict_enery():
+    #3. Fetching Information from NASA's InSight MARS API
+    resp = requests.get("https://api.nasa.gov/insight_weather/?api_key="+NASA_API_KEY+"&feedtype=json&ver=1.0")
+    resp.raise_for_status() 
+
+    #4. Storing those Information into the IBM Cloudant
+    # Setting Up Authenticator with the IBm Cloud API Key
+    authenticator = IAMAuthenticator(CLOUDANT_API_KEY)
+    # Initializing the Cloudant Client with the Service URL 
+    client = CloudantV1(authenticator=authenticator)
+    client.set_service_url(CLOUDANT_SERVICE_URL)
+
+    #5. Inserting Documents in the Cloudant
+    try:
+        # This Generates an Unique Server-Side Document ID Automatically
+        response = client.post_document(
+                db=CLOUDANT_DB,
+                document=resp.json()
+            ).get_result()
+        print("Success! Document inserted.")
+    
+    #6. Capturing the Exceptions
+    except ApiException as ae:
+        print(f"IBM Cloudant API exception occurred: {ae.code} - {ae.message}")
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
+    
+    #7. Init Client + Picking Up a Granite Model for the account
+    client = APIClient(credentials=creds, project_id=project_id)
+    params = {
+    GenParams.MAX_NEW_TOKENS: 300,
+    GenParams.TEMPERATURE: 0.2,     # low temp for more factual outputs
+    GenParams.TOP_P: 0.9,
+    }
+    model = ModelInference(
+    model_id="ibm/granite-4-h-small",  # or another Granite instruct model you have access to
+    params=params,
+    credentials=creds,
+    project_id=project_id,
+    )
+   
+    #8. Prompt: using a JSON and returning
+    prompt=f"""
+    You are a space scientist. So, could you please analyze the data {resp.json()} and predict the energy optimization in Mars?
+    """
+    res = model.generate(prompt=prompt)
+    print(res["results"][0]["generated_text"])
+    return res
+    
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=5000, debug=True)
