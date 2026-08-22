6cf99dac403ab3bed9bea2cc01d430d83e4386e4
Nasim-Hasan-IBM <nhasan50@hotmail.com>
2026-08-23 02:02:31 +0600

    Modified the Front-End (HTML) + Back-End (Python) Scripts for the OpenAI (GPT-5) Model

--- api/Mars_Greenhouse.py
+++ api/Mars_Greenhouse.py
@@ -3,6 +3,7 @@
 # pip install ibmcloudant
 # pip install flask flask-cors
 # pip install -U ibm-watsonx-ai python-dotenv
+# pip install openai
 import os
 import requests
 from dotenv import load_dotenv
@@ -14,6 +15,7 @@ from ibm_cloud_sdk_core import ApiException
 from ibm_watsonx_ai import Credentials, APIClient
 from ibm_watsonx_ai.foundation_models import ModelInference
 from ibm_watsonx_ai.metanames import GenTextParamsMetaNames as GenParams
+from openai import OpenAI
 
 app = Flask(__name__)
 CORS(app)  # remove or restrict in production
@@ -24,28 +26,32 @@ NASA_API_KEY = os.environ["NASA_API_KEY"]
 CLOUDANT_API_KEY = os.environ["CLOUDANT_API_KEY"]
 CLOUDANT_SERVICE_URL = os.environ["CLOUDANT_SERVICE_URL"]
 CLOUDANT_DB = os.environ["CLOUDANT_DB"]
-WATSONX_API_KEY = os.environ["WATSONX_API_KEY"]
-WATSONX_URL = os.environ["WATSONX_URL"]
-WATSONX_PROJECT_ID = os.environ["WATSONX_PROJECT_ID"]
-creds = Credentials(
-api_key=os.environ["WATSONX_API_KEY"],
-url=os.environ["WATSONX_URL"],
-)
-project_id = os.environ["WATSONX_PROJECT_ID"]
+#Connection Process for IBM WatsonX AI Model (granite-4-h-small)
+# WATSONX_API_KEY = os.environ["WATSONX_API_KEY"]
+# WATSONX_URL = os.environ["WATSONX_URL"]
+# WATSONX_PROJECT_ID = os.environ["WATSONX_PROJECT_ID"]
+# creds = Credentials(
+# api_key=os.environ["WATSONX_API_KEY"],
+# url=os.environ["WATSONX_URL"],
+# )
+# project_id = os.environ["WATSONX_PROJECT_ID"]
 
-#2. Init Client + Picking Up a Granite Model for the account
-client = APIClient(credentials=creds, project_id=project_id)
-params = {
-GenParams.MAX_NEW_TOKENS: 300,
-GenParams.TEMPERATURE: 0.2,     # low temp for more factual outputs
-GenParams.TOP_P: 0.9,
-}
-model = ModelInference(
-model_id="ibm/granite-4-h-small",  # or another Granite instruct model you have access to
-params=params,
-credentials=creds,
-project_id=project_id,
-)
+# #2. Init Client + Picking Up a Granite Model for the account
+# client = APIClient(credentials=creds, project_id=project_id)
+# params = {
+# GenParams.MAX_NEW_TOKENS: 300,
+# GenParams.TEMPERATURE: 0.2,     # low temp for more factual outputs
+# GenParams.TOP_P: 0.9,
+# }
+# model = ModelInference(
+# model_id="ibm/granite-4-h-small",  # or another Granite instruct model you have access to
+# params=params,
+# credentials=creds,
+# project_id=project_id,
+# )
+
+#Connection Process for the OpenAI (GPT-5) Model
+openaiclient = OpenAI()
 
 #3-a. Predict Irrigation in MARS
 @app.get("/api/irrigation")
@@ -81,10 +87,17 @@ def predict_irrigation():
     prompt=f"""
     You are a space scientist. So, could you please analyze the data {resp.json()} and predict the irrigation in Mars?
     """
-    res = model.generate(prompt=prompt)
-    #print(res)
-    print(res["results"][0]["generated_text"])
-    return res
+    #Response from IBM WatsonX AI (granite-4-h-small) Model
+    #res = model.generate(prompt=prompt)
+
+    #Response from OpenAI (gpt-5) Model
+    res = openaiclient.responses.create(
+    model="gpt-5",
+    input= prompt
+    )
+    print(res.output_text)
+    #print(res["results"][0]["generated_text"])
+    return res.output_text
 
 #3-b. Predict Diseases in MARS
 @app.get("/api/disease")
@@ -119,9 +132,17 @@ def predict_disease():
     prompt=f"""
     You are a space scientist. So, could you please analyze the data {resp.json()} and predict the diseases in Mars?
     """
-    res = model.generate(prompt=prompt)
-    print(res["results"][0]["generated_text"])
-    return res
+    #Response from IBM WatsonX AI (granite-4-h-small) Model
+    #res = model.generate(prompt=prompt)
+    
+    #Response from OpenAI (gpt-5) Model
+    res = openaiclient.responses.create(
+    model="gpt-5",
+    input= prompt
+    )
+    print(res.output_text)
+    #print(res["results"][0]["generated_text"])
+    return res.output_text
 
 #3-c. Predict Energy Optimization in MARS
 @app.get("/api/energy")
@@ -156,9 +177,17 @@ def predict_enery():
     prompt=f"""
     You are a space scientist. So, could you please analyze the data {resp.json()} and predict the energy optimization in Mars?
     """
-    res = model.generate(prompt=prompt)
-    print(res["results"][0]["generated_text"])
-    return res
+    #Response from IBM's WatsonX AI (granite-4-h-small) Model
+    #res = model.generate(prompt=prompt)
+    
+    #Response from OpenAI (gpt-5) Model
+    res = openaiclient.responses.create(
+    model="gpt-5",
+    input= prompt
+    )
+    print(res.output_text)
+    #print(res["results"][0]["generated_text"])
+    return res.output_text
     
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=5000, debug=True)
