26e1a33c904c4f809cb765e98adbdc1e967a5315
Nasim-Hasan <nasimcse2k5@gmail.com>
2026-08-24 17:43:58 +0600

    Again Modification for the Production

--- app.py
+++ app.py
@@ -208,4 +208,4 @@ def predict_enery():
 if __name__ == "__main__":
   # Bind to 0.0.0.0 and use the port provided by Render
   port = int(os.environ.get("PORT", 5000))
-  app.run(host="127.0.0.1", port=port, debug=True)
+  app.run(host="0.0.0.0", port=port, debug=True)
